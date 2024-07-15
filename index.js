e05491cac54347194c6356d106394a34fb49aee3
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-15 15:24:20 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,45 +1,23 @@
-// Code your solution in this file!
-
-const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
-
-function returnFirstTwoDrivers(drivers) {
+const returnFirstTwoDrivers = function (drivers) {
     return drivers.slice(0, 2);
-}
-
-function returnLastTwoDrivers(drivers) {
+  };
+  
+  const returnLastTwoDrivers = function (drivers) {
     return drivers.slice(-2);
-}
-
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
-function selectingDrivers(drivers) {
-    return [returnFirstTwoDrivers, returnLastTwoDrivers];
-}
-
-function createFareMultiplier(multiplier) {
-    return function (fare) {
-        return fare * multiplier;
-    }
-}
-
-const fareDoubler = createFareMultiplier(2);
-const fareTripler = createFareMultiplier(3);
-const fareHaverser = createFareMultiplier(0.5);
-const fareQuintupler = createFareMultiplier(5);
-
-function fareDoubler(fare) {
-    return fare * 2;
-}
-const fareTripler = createFareMultiplier(3);
-
-function fareTripler(fare) {
-    return fare * 3;
-}
-
-function fareHaverser(fare) {
-    return fare * 0.5;
-}
-
-function fareQuintupler(fare) {
-    return fare * 5;
-}
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+  };
